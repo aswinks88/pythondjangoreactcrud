fe30798763b3f54144d822bc969805c5f82292f0
aswinks88 <aswinks88@gmail.com>
2020-08-04 01:10:28 +1200

    added required components

---
+++ leadmanager/frontend/src/component/leads/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Form extends Component {
+    render() {
+        return (
+            <div>
+                <h1> add lead form</h1>
+            </div>
+        )
+    }
+}
+
+export default Form
